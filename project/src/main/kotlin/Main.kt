3d96b6befad23d50116cd27147af70dd015324ba
nathalia-bnka <nathalia.lopez@bnka.com>
2026-07-07 12:05:47 -0500

    End lesson

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,40 @@
 
 fun main() {
-    
+    val aChar: Char = 'a'
+    val aNumericChar = '1'
+    val newLineChar = '\n'
+
+    //val concatOption1 = 'a' + 'b'
+
+    //val concatOption2 = 'ab'
+
+    val aString: String = "Hello Kotlin!"
+    val escaped = "Hello \n World"
+    println(escaped)
+
+    val raw = """
+     >>some
+        multiline
+    text
+        >>is going on 
+        here
+    """.trimMargin(marginPrefix = ">>")
+
+    println(raw)
+
+    val concatString = "Hello" + "Kotlin!"
+
+    val concatNumber = "The number " + 10
+    println(concatNumber)
+
+    val aNumber = 10
+    val concatTemplate = "The number ${1 + aNumber}"
+    println(concatTemplate)
+
+
+    val someString = "   "
+    println(someString.isNotBlank())
+
+    val anotherString = "Hello Kotlin World!"
+    anotherString.contains("Kotlin")
 }
